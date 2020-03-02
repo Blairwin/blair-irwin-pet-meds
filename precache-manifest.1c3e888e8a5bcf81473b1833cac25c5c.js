57b94f1b210d8d2e2a8704206f128f1b15d73a43
Blairwin <reach.blair@gmail.com>
2020-03-02 16:51:13 -0500

    Updates

---
+++ precache-manifest.1c3e888e8a5bcf81473b1833cac25c5c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ed8a7010b466ea47955a7a6b693ef4",
+    "url": "/blair-irwin-project-5/index.html"
+  },
+  {
+    "revision": "fd43685ab16b63151f18",
+    "url": "/blair-irwin-project-5/static/css/2.97516602.chunk.css"
+  },
+  {
+    "revision": "de29e2a5aa9c84f01bc6",
+    "url": "/blair-irwin-project-5/static/css/main.6672efb1.chunk.css"
+  },
+  {
+    "revision": "fd43685ab16b63151f18",
+    "url": "/blair-irwin-project-5/static/js/2.fae7b642.chunk.js"
+  },
+  {
+    "revision": "80f88c8a2fc44f425b1cf753399d693e",
+    "url": "/blair-irwin-project-5/static/js/2.fae7b642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de29e2a5aa9c84f01bc6",
+    "url": "/blair-irwin-project-5/static/js/main.3de305a3.chunk.js"
+  },
+  {
+    "revision": "70126cf4b6c0ba92e918",
+    "url": "/blair-irwin-project-5/static/js/runtime-main.049ad5c1.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/blair-irwin-project-5/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/blair-irwin-project-5/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/blair-irwin-project-5/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/blair-irwin-project-5/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/blair-irwin-project-5/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
